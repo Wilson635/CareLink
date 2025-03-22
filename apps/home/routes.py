fe21b2d208cbd4c99f416a3382835246ab177f18
Wilson635 <87822542+Wilson635@users.noreply.github.com>
2025-03-22 13:19:24 +0100

    Implémentation des services de gestions d'un hôpital et mise en place du template pour le développement

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -8,59 +8,82 @@ from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
 
+
 @blueprint.route('/index')
 @login_required
 def index():
     return render_template('pages/index.html', segment='index')
 
-@blueprint.route('/typography')
-@login_required
-def typography():
-    return render_template('pages/typography.html')
 
-@blueprint.route('/color')
+@blueprint.route('/chambres')
 @login_required
-def color():
-    return render_template('pages/color.html')
+def chambres():
+    return render_template('pages/chambres.html')
 
-@blueprint.route('/icon-tabler')
-@login_required
-def icon_tabler():
-    return render_template('pages/icon-tabler.html')
 
-@blueprint.route('/sample-page')
+@blueprint.route('/hospitalisation')
 @login_required
-def sample_page():
-    return render_template('pages/sample-page.html')  
+def hospitalisation():
+    return render_template('pages/hospitalisation.html')
+
+
+@blueprint.route('/consultation')
+@login_required
+def consultation():
+    return render_template('pages/consultation.html')
+
+
+@blueprint.route('/medecin')
+@login_required
+def medecin():
+    return render_template('pages/medecin.html')
+
+
+@blueprint.route('/infirmière')
+@login_required
+def infirmiere():
+    return render_template('pages/infirmiere.html')
+
+
+@blueprint.route('/patients')
+@login_required
+def patients():
+    return render_template('pages/patients.html')
+
 
 @blueprint.route('/accounts/password-reset/')
 def password_reset():
     return render_template('accounts/password_reset.html')
 
+
 @blueprint.route('/accounts/password-reset-done/')
 def password_reset_done():
     return render_template('accounts/password_reset_done.html')
+
 
 @blueprint.route('/accounts/password-reset-confirm/')
 def password_reset_confirm():
     return render_template('accounts/password_reset_confirm.html')
 
+
 @blueprint.route('/accounts/password-reset-complete/')
 def password_reset_complete():
     return render_template('accounts/password_reset_complete.html')
+
 
 @blueprint.route('/accounts/password-change/')
 def password_change():
     return render_template('accounts/password_change.html')
 
+
 @blueprint.route('/accounts/password-change-done/')
 def password_change_done():
     return render_template('accounts/password_change_done.html')
 
+
 @blueprint.route('/<template>')
 @login_required
 def route_template(template):
-
     try:
 
         if not template.endswith('.html'):
@@ -78,9 +101,9 @@ def route_template(template):
     except:
         return render_template('home/page-500.html'), 500
 
+
 # Helper - Extract current page name from request
 def get_segment(request):
-
     try:
 
         segment = request.path.split('/')[-1]
